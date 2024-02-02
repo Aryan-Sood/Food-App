6b0f0d4f03cf051f0a47cef9c5326c90f08a2ac3
Aryan <aryansood12@gmail.com>
2024-02-03 02:24:06 +0530

    search bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,62 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          searchBar(),
+        ],
+      ),
+    );
+  }
+
+  Container searchBar() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Color(0xff1D1617).withOpacity(0.11),
+          blurRadius: 40,
+          spreadRadius: 0.0,
+        )
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: SvgPicture.asset("assets/icons/search.svg"),
+            ),
+            suffixIcon: Container(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    VerticalDivider(
+                      color: Colors.black,
+                      thickness: 0.1,
+                      indent: 10,
+                      endIndent: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10, right: 15, top: 10, bottom: 10),
+                      child: SvgPicture.asset("assets/icons/filter.svg"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            contentPadding: EdgeInsets.all(15),
+            hintText: "Search",
+            hintStyle: TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none,
+            )),
+      ),
     );
   }
 
